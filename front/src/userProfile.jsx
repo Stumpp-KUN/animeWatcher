7ef741896c4b11a31c9762dd0b1068a429740e40
Stumpp <kiril.bychkov.03@mail.ru>
2023-07-24 23:00:23 +0300

    made edit button functionality, now, user can change fn,ln

--- front/src/userProfile.jsx
+++ front/src/userProfile.jsx
@@ -12,6 +12,17 @@ const UserProfile = () => {
   const [userInfo, setUserInfo] = useState(null);
   const [image1, setImage1] = useState(null);
   const history = useHistory();
+  const [editedFirstname, setEditedFirstname] = useState('');
+  const [editedLastname, setEditedLastname] = useState('');
+  const [isEditing, setIsEditing] = useState(false); // Track if editing mode is enabled
+
+  const handleFirstnameChange = (event) => {
+    setEditedFirstname(event.target.value);
+  };
+
+  const handleLastnameChange = (event) => {
+    setEditedLastname(event.target.value);
+  };
 
   const handleRedirect = () => {
     history.push('/');
@@ -43,6 +54,45 @@ const UserProfile = () => {
       await fetchUserInfo();
     } catch (error) {
       console.error('Failed to upload photo:', error);
+    }
+  };
+
+  const handleSave = async () => {
+    try {
+      setUserInfo((prevUserInfo) => ({
+        ...prevUserInfo,
+        firstname: editedFirstname,
+        lastname: editedLastname,
+      }));
+      
+      setEditedFirstname("");
+      setEditedLastname("");
+      setIsEditing(false);
+
+      if (accessToken && userId && editedFirstname.trim() !== '' && editedLastname.trim() !== '') {
+        const editedData = {
+          id: userId,
+          firstname: editedFirstname,
+          lastname: editedLastname,
+          email: userInfo.email,
+          role: userInfo.role,
+        };
+  
+        await axios.put(`http://localhost:8080/api/v1/edit/${userId}`, editedData, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+  
+        console.log('Data update successful');
+        await fetchUserInfo();
+         
+      } else {
+        alert('Firstname and lastname cannot be empty');
+      }
+      
+    } catch (error) {
+      console.error('Failed to update data:', error);
     }
   };
 
@@ -96,29 +146,57 @@ const UserProfile = () => {
         <div className="profile-info">
           {userInfo ? (
             <div>
-              <p className="profile-item">
-                <span className="profile-label">Firstname</span> {userInfo.firstname}
-              </p>
-              <p className="profile-item">
-                <span className="profile-label">Lastname</span> {userInfo.lastname}
-              </p>
-              <p className="profile-item">
+              {isEditing ? (
+                <>
+                  <div className="error-item">
+                    <div>
+                    <label className='edit-field'>Firstname</label>
+                    <input type="text" value={editedFirstname} onChange={handleFirstnameChange} />
+                    </div>
+
+                    <div>
+                    <label className='edit-field'>Lastname:</label>
+                    <input type="text" value={editedLastname} onChange={handleLastnameChange} />
+                    </div>
+
+                    <input type="file" name="file1" onChange={handleFileChange} />
+                    <button className="button-7" role="button" onClick={handleUpload}>Upload Photo</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p className="profile-item">
+                    <span className="profile-label">Firstname</span> {userInfo.firstname}
+                  </p>
+                  <p className="profile-item">
+                    <span className="profile-label">Lastname</span> {userInfo.lastname}
+                  </p>
+                  <p className="profile-item">
                 <span className="profile-label">Email</span> {userInfo.email}
               </p>
-              <p className="profile-item">
+                <p className="profile-item">
                 <span className="profile-label">Role</span> {userInfo.role}
               </p>
+                </>
+              )}
             </div>
           ) : (
             <p>Loading...</p>
           )}
         </div>
         <div className='buttons'>
-          <button className="button-7" role="button" onClick={handleDeleteProfile}>Delete</button>
-          <button className="button-7" role="button">Edit</button>
-          <input type="file" name="file1" onChange={handleFileChange} />
-          <button className="button-7" role="button" onClick={handleUpload}>Upload Photo</button>
-        </div>
+          {isEditing ? (
+            <>
+              <button className="button-7" role="button" onClick={handleSave}>Save</button>
+              <button className="button-7" role="button" onClick={() => setIsEditing(false)}>Cancel</button>
+            </>
+          ) : (
+            <>
+              <button className="button-7" role="button" onClick={handleDeleteProfile}>Delete</button>
+              <button className="button-7" role="button" onClick={() => setIsEditing(true)}>Edit</button>
+            </>
+          )}
+        </div>      
       </div>
     </div>
   );
